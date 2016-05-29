be888c6d5c5cd70d7fa8feb364867c80e5476d23
Daniel Nuno <daniel.nuno@gmail.com>
2016-05-28 23:52:49 -0500

    pruebas

--- src/github.com/Guanatos/hello/hello.go
+++ src/github.com/Guanatos/hello/hello.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/Guanatos/stringutil"
+)
 
 func main() {
-	fmt.Printf("Hello, World\n")
+	fmt.Printf(stringutil.Reverse("Hello, Daniel\n"))
 }
