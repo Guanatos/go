5beb6540265b31a4f36a984356680c0b2d2bcb14
Daniel Nuno <daniel.a.nuno.ramirez@intel.com>
2016-05-30 14:49:25 -0500

    variables & types

--- src/github.com/Guanatos/hello/variables.go
+++ src/github.com/Guanatos/hello/variables.go
@@ -14,11 +14,19 @@ var (
 	varType5 = "cinco" 
 	varType6 = "seis"
 )
+//
+const (
+	varType7 = "Test7"
+	varType8 = "Test8"
+)
 
 
 func main() {
+	varType9 := "Test9"
 	fmt.Println(varType1)
 	fmt.Println(varType2)
 	fmt.Println(varType3, varType4)
 	fmt.Println(varType5, varType6)
+	fmt.Println(varType7, varType8)
+	fmt.Println(varType9)
 }
