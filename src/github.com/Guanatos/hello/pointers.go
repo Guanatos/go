6ab8e1b75cf11b64b1b75b9dab22ed26cc207712
Daniel Nuno <daniel.a.nuno.ramirez@intel.com>
2016-06-01 14:53:36 -0500

    pointers

--- src/github.com/Guanatos/hello/pointers.go
+++ src/github.com/Guanatos/hello/pointers.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 var shippingDays = 30
+var shippingDaysPtr = new(int)
 
 func main(){
      shippingDaysAdjust(shippingDays)
@@ -12,12 +13,42 @@ func main(){
      fmt.Println("after shipping Days Adjustments Ptr call",shippingDays)
      fmt.Println("after shipping Days Adjustments Ptr call",&shippingDays)
 
+     shipper := shipper{}
+     shipper.name = "Datsun"
+     shipper.id = 400
+
+     fmt.Println("Shippers: ",shipper)
+
+     shipperUpdates(&shipper)
+
+     fmt.Println("Shippers Id: ",shipper.id)
+     fmt.Println("Shippers Name: ",shipper.name)
+
+//   Pointers
+     *shippingDaysPtr = 55
+
+//   We are not using & as it's already a pointer
+     shippingDaysAdjustPtr(shippingDaysPtr)
+     fmt.Println("shippingDaysPtr after",*shippingDaysPtr)
 }
 
+// Copy of the variable
 func shippingDaysAdjust(shippingDays int){
      shippingDays += 10
 }
 
+// Pointer
 func shippingDaysAdjustPtr(shippingDays *int){
      *shippingDays += 10
+}
+
+// Pointer to an structure
+func shipperUpdates(s *shipper){
+     s.id += 10
+     s.name += " Inc."
+}
+
+type shipper struct{
+     name string
+     id int
 }
