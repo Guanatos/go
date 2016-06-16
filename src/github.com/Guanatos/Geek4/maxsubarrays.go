588b6d5888a1a1a3451510e5e894ced94e5f1e43
Daniel Nuno <daniel.a.nuno.ramirez@intel.com>
2016-06-16 16:06:35 -0500

    final geek4geeks

--- src/github.com/Guanatos/Geek4/maxsubarrays.go
+++ src/github.com/Guanatos/Geek4/maxsubarrays.go
@@ -2,23 +2,27 @@ package main
 
 import "fmt"
 
+// greater than (>) or less than (<)
+
 func main() {
-	a := [4]int{4,1,3,2}
+	a := [4]int{4,3,2,1}
 	fmt.Println("Elements: ",a)
 	n := len(a)
 	fmt.Println("Lenght (n): ",n)
 	k := 3
 	fmt.Println("Subarray Lenght (k): ",k)
+	fmt.Println("########")
 	for i := 0; i <= n-k; i++ {
 	    fmt.Println("Value (i): ",i)
-	    fmt.Println("Element: ",a[i])
 	    max := a[i]
+	    fmt.Println("Element a[i]: ",a[i])
 	    fmt.Println("Inital Max: ",max)
 	    for j := 1; j < k; j++ {
-	    	fmt.Println("Value (j < k): ",j<k)
-	    	fmt.Println("Value (j): ",j)
+	    	fmt.Println("  Value (j): ",j)
+	    	fmt.Println("  Value (k): ",k)
+	    	fmt.Println("  Value (j < k): ",j<k)
                 if (a[i+j] > max){
-			fmt.Println("Value (a[i+j] > max:",a[i+j] > max)
+			fmt.Println("Value (a[i+j] > max): ",a[i+j] > max)
 			max = a[i+j]
 	                fmt.Println("New Max: ",max)
 		    }
